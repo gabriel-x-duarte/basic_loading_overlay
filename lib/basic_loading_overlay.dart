506d45fc2958b4fff697dbf23dedea0748ebf20f
gabriel-duarte <gabrielxavierduarte@gmail.com>
2021-10-31 12:56:40 -0300

    incluindo loading overlay

--- lib/basic_loading_overlay.dart
+++ lib/basic_loading_overlay.dart
@@ -1,7 +1,28 @@
 library basic_loading_overlay;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+/// A loading overlays.
+
+class BasicLoadingOverlay extends StatelessWidget {
+  const BasicLoadingOverlay({
+    this.backgroundColor = Colors.transparent,
+    this.loadingIndicatorColor = Colors.deepOrangeAccent,
+    Key? key,
+  }) : super(key: key);
+
+  final Color backgroundColor;
+  final Color loadingIndicatorColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: backgroundColor,
+      child: Center(
+        child: CircularProgressIndicator(
+          color: loadingIndicatorColor,
+        ),
+      ),
+    );
+  }
 }
--- test/basic_loading_overlay_test.dart
+++ test/basic_loading_overlay_test.dart
@@ -3,10 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:basic_loading_overlay/basic_loading_overlay.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('Basic loading overlay', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const BasicLoadingOverlay());
   });
 }
